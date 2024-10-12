a6ccf6056e5048d7e7b53590f815ecb47e0e2f88
Javier Cabrera <fcabreraclaros@gmail.com>
2024-10-12 13:46:45 -0400

    index fixed

--- src/admin/dto/index.ts
+++ src/admin/dto/index.ts
@@ -3,9 +3,6 @@ export * from './createPersonal.dto';
 export * from './createMascota.dto';
 export * from './createPersonal.dto';
 
-export * from './getPersonal.dto';
-export * from './getQuery.dto';
-
 export * from './updateCliente.dto';
 export * from './updateMascota.dto';
 export * from './updatePersonal.dto';
