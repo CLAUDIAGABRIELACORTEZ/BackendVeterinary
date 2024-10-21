d07c039fd6d1bc7fd188e885e2ee29be8ec8464e
Javier Cabrera <fcabreraclaros@gmail.com>
2024-10-21 17:05:49 -0400

    get mascotas de cliente modificado

--- src/client/client.service.ts
+++ src/client/client.service.ts
@@ -20,10 +20,20 @@ export class ClientService {
             }
         });
         await registrarEnBitacora(this.prisma, userId, BitacoraAccion.ListarMascotas, ipDir);
-        return await this.prisma.mascota.findMany({
-            where: {
-                ClienteID: cliente.ClienteID
-            }
-        });
+        return this.prisma.$queryRaw`
+            SELECT 
+              m."MascotaID" AS "ID",
+              m."Nombre",
+              m."Sexo",
+              TO_CHAR(m."FechaNacimiento", 'YYYY-MM-DD') AS "Fecha_De_Nacimiento",
+              e."NombreEspecie" AS "Especie",
+              r."NombreRaza" AS "Raza"
+            FROM mascota m
+            JOIN raza r ON m."RazaID" = r."RazaID"
+            JOIN especie e ON r."EspecieID" = e."EspecieID"
+            JOIN cliente c ON m."ClienteID" = c."ClienteID"
+            WHERE c."ClienteID" = ${cliente.ClienteID}
+            ORDER BY m."MascotaID" ASC;
+        `;
     }
 }
